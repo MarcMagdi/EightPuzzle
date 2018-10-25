28ef7b52c9ae09f68f244f2fc3ffbe1fb8a96968
Marc Magdi <mavloper@gmail.com>
2018-10-25 13:11:20 +0200

    Adding Click Event

--- src/com/bmi/ai/controllers/SolvingAreaController.java
+++ src/com/bmi/ai/controllers/SolvingAreaController.java
@@ -1,6 +1,13 @@
 package com.bmi.ai.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,8 +20,23 @@ import java.util.ResourceBundle;
  * Friday 25 October 2018
  */
 public class SolvingAreaController implements Initializable {
+
+
+    @FXML
+    private GridPane gridPane;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("hello world");
+        gridPane.add(new Label("test"), 2, 2, 1, 1);
+    }
+
+    @FXML
+    private void clickGrid(MouseEvent event) {
+        Node source = (Node)event.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+
+        System.out.println(colIndex + ' ' + rowIndex);
     }
 }
